b407e64c101a46cf61da6aebc710eb5c2595c489
Vladyslav Surin <surinvladyslav@gmail.com>
2022-10-21 23:40:54 +0300

    Initial Commit

---
+++ TelegramContest/TelegramContestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramContestApp.swift
+//  TelegramContest
+//
+//  Created by Vladyslav Surin on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TelegramContestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
